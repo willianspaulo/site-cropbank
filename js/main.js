8e4afd4362814a3792363c1b99d05c75d069e82b
Willians Paulo da Silva <willianspaulo@live.com>
2021-05-20 22:36:45 -0300

    .

--- js/main.js
+++ js/main.js
@@ -124,27 +124,27 @@ Project Carousel
     /*----------------------------
     Count Down
     ------------------------------ */
-     $('.countdown').downCount({
-     date: '12/12/2019 12:00:00'
-    });
+    //  $('.countdown').downCount({
+    //  date: '12/12/2019 12:00:00'
+    // });
 
      /* Partical Bg */
-    var count_particles, stats, update;
-    stats = new Stats;
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0px';
-    stats.domElement.style.top = '0px';
-    document.body.appendChild(stats.domElement);
-    count_particles = document.querySelector('.js-count-particles');
-    update = function() {
-        stats.begin();
-        stats.end();
-        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-        }
-        requestAnimationFrame(update);
-    };
-    requestAnimationFrame(update);
+    // var count_particles, stats, update;
+    // stats = new Stats;
+    // stats.setMode(0);
+    // stats.domElement.style.position = 'absolute';
+    // stats.domElement.style.left = '0px';
+    // stats.domElement.style.top = '0px';
+    // document.body.appendChild(stats.domElement);
+    // count_particles = document.querySelector('.js-count-particles');
+    // update = function() {
+    //     stats.begin();
+    //     stats.end();
+    //     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    //         count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    //     }
+    //     requestAnimationFrame(update);
+    // };
+    // requestAnimationFrame(update);
 
 })(jQuery); 
